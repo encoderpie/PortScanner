6bc8d246758bfc64f0236839b26effb2a2ee6020
encoderpie <84232289+encoderpie@users.noreply.github.com>
2021-11-27 00:02:28 +0300

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,11 +1,13 @@
 from socket import *
 from threading import *
 import optparse
+import os
 
 class colors:
-  G = '\033[92m'
-  R = '\033[91m'
+  G = '\033[92m' # Green
+  R = '\033[91m' # Red
   ENDC = '\033[0m'
+  C = '\033[96m' # Cyan
 
 class defaultPorts:
   dp = ['20','21','22','23','25','53','67','68','80','161','162','123','443','7104','7102','7105']
@@ -44,7 +46,7 @@ def hostScan(targetHost,targetPorts):
     print(colors.R + " " + "-"*18, "-"*stickLen + colors.ENDC, sep="")
     print(infoG + "Port(s) Scanning...")
   except:
-    print(infoR + "Hostname not found: " + str(targetHost))
+    print(colors.R + "\n [INFO] " + colors.ENDC + "Hostname not found: " + str(targetHost))
     exit(0)
 
   for targetPort in targetPorts:
@@ -71,8 +73,29 @@ def portScan(targetHost,targetPort,cleanConsole):
     finally:
       sock.close()
 
+def banner():
+  banner = ("""{}  _____           _      _____                                 
+ |  __ \         | |    / ____|                                
+ | |__) |__  _ __| |_  | (___   ___ __ _ _ __  _ __   ___ _ __
+ |  ___/ _ \| '__| __|  \___ \ / __/ _` | '_ \| '_ \ / _ \ '__|{}
+ | |  | (_) | |  | |_   ____) | (_| (_| | | | | | | |  __/ |{}
+ |_|   \___/|_|   \__| |_____/ \___\__,_|_| |_|_| |_|\___|_|{}""".format(colors.C, colors.G, colors.R, colors.ENDC))
+  print(banner)
+
+class parserOp:
+  text = (colors.G + " " + "-"*59 + colors.C + 
+  """\n Command usage: python3 scan.py [option] arg,arg
+ Opitons:
+   -H <host address, ex: 192.168.1.30 or example.com>
+   -p <port, ex: 80 or 80,144,265>
+   -r <port range, ex: 80,1024>\n
+ Command examples:
+   -H and -p usage ex: python3 scan.py -H 192.168.1.30 -p 80
+   -H and -r usage ex: python3 scan.py -H google.com -r 20,90\n""" + colors.G + " " + "-"*59 + colors.ENDC)
+
 def main():
-  parser = optparse.OptionParser("Command usage: python3 scan.py [option] arg [option] arg\nOpitons:\n  -H <host address, ex: 192.168.1.30 or example.com>\n  -p <port, ex: 80 or 80,144,265>\n  -r <port range, ex: 80,1024>\n\nCommand examples:\n  -H and -p usage ex: python3 scan.py -H 192.168.1.30 -p 80\n  -H and -r usage ex: python3 scan.py -H google.com -r 20,90")
+  banner()
+  parser = optparse.OptionParser(parserOp.text)
   parser.add_option("-H", dest="targetHost", type="string", help="The web address or IP address to be scanned.")
   parser.add_option("-p", dest="targetPort", type="string", help="The port address(es) of the entered address to be scanned.")
   parser.add_option("-r", dest="targetPortRange", type="string", help="The port address range of the entered address to be scanned.")
@@ -80,38 +103,44 @@ def main():
   targetHost = options.targetHost
   targetPortMain = str(options.targetPort).split(",")
   targetPortRange = str(options.targetPortRange).split(",")
-  try:
-    if targetHost == None:
-      exit(0)
-    else:
-      if targetPortMain != ['None'] and targetPortRange != ['None']:
-        exit(0)
-      else:
-        if targetPortMain == ['None']:
-          if targetPortRange == ['None']:
-            targetPorts = defaultPorts.dp
-          elif targetPortRange[1] == ['None']:
-            exit(0)
-          else:
-            portRange = []
-            if targetPortRange[0]>targetPortRange[1]:
-              tpr1 = int(targetPortRange[0])+1
-              tpr2 = int(targetPortRange[1])
-              while tpr1 != tpr2:
-                portRange.append(tpr2)
-                tpr2=tpr2+1
-            elif targetPortRange[0]<targetPortRange[1]:
-              tpr1 = int(targetPortRange[0])
-              tpr2 = int(targetPortRange[1])+1
-              while tpr1 != tpr2:
-                portRange.append(tpr1)
-                tpr1=tpr1+1
-            targetPorts = portRange
-        else:
-          targetPorts = targetPortMain
-  except:
+
+  if targetHost == None:
     print(parser.usage)
     exit(0)
+  else:
+    if "https://" in targetHost:
+      targetHost = targetHost[8:]
+    if "http://" in targetHost:
+      targetHost = targetHost[7:]
+    if "/" in targetHost:
+      targetHost = targetHost[:-1]
+    if targetPortMain != ['None'] and targetPortRange != ['None']:
+      print(parser.usage)
+      exit(0)
+    else:
+      if targetPortMain == ['None']:
+        if targetPortRange == ['None']:
+          targetPorts = defaultPorts.dp
+        elif targetPortRange[1] == ['None']:
+          print(parser.usage)
+          exit(0)
+        else:
+          portRange = []
+          if targetPortRange[0]>targetPortRange[1]:
+            tpr1 = int(targetPortRange[0])+1
+            tpr2 = int(targetPortRange[1])
+            while tpr1 != tpr2:
+              portRange.append(tpr2)
+              tpr2=tpr2+1
+          elif targetPortRange[0]<targetPortRange[1]:
+            tpr1 = int(targetPortRange[0])
+            tpr2 = int(targetPortRange[1])+1
+            while tpr1 != tpr2:
+              portRange.append(tpr1)
+              tpr1=tpr1+1
+          targetPorts = portRange
+      else:
+        targetPorts = targetPortMain
 
   hostScan(targetHost,targetPorts)
 
